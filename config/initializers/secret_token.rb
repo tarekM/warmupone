3e1abe36c8d3465924fa9f92e2a00e58a82dd8a1
TarekM <tal-mouhtasseb@berkeley.edu>
2014-02-09 18:35:47 -0800

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Warmupone::Application.config.secret_key_base = '16259d23d5588790f2e4e09ec4a493d44a92b96e5281d4645091248e8920b9457a539dc8e45519f23bbddd96d35ca080e09fdb36deb58218fee3988c9b61c868'
